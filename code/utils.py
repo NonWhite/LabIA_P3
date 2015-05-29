d848490ae8cf8967e5fe3a7c504b332803c7a81e
NonWhite <wperezurcia@gmail.com>
2015-05-29 01:04:35 -0300

    added method for verifying is string is an integer

--- code/utils.py
+++ code/utils.py
@@ -5,3 +5,6 @@ def getAllValues( lstDictionary , key ) :
 
 def formProposition( name , state , time , isaction ) :
 	return { 'name' : name , 'state' : state , 'time' : time , 'isaction' : isaction }
+
+def isnumber( s ) :
+	return s[ 1: ].isdigit() if s.startswith( '-' ) else s.isdigit()
